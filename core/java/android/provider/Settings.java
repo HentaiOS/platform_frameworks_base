7ec983cfd41cf1e0f3111390b44449890083807d
Dyneteve <dyneteve@hentaios.com>
2024-03-20 21:05:52 +0000

    treewide: Introduce hentai-utils and rename gmscompat
    This for now supports:
    - Updatable device config/spoofed device via app update
    - Spoofing Google Apps
    - Override already existing device configuration, this will allow us to enable stuff like Circle to Search.
    
    Planned:
    - Add support for spoofing device features.
    - Move framework to it's own apex that is updatable from our OTA app.
    
    Change-Id: Id666d629e145bf7d78b1bd61c2f1be6ca22f27ec
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -118,6 +118,8 @@ import java.util.Set;
 import java.util.concurrent.Executor;
 import java.util.function.Consumer;
 
+import com.android.internal.hentaiutils.HentaiConfigUtils;
+
 /**
  * The Settings provider contains global system-level device preferences.
  */
@@ -19811,7 +19813,18 @@ public final class Settings {
         @RequiresPermission(Manifest.permission.WRITE_DEVICE_CONFIG)
         public static boolean putString(@NonNull String namespace,
                 @NonNull String name, @Nullable String value, boolean makeDefault) {
+            Context context = ActivityThread.currentApplication().getApplicationContext();
             ContentResolver resolver = getContentResolver();
+
+            // Check if the value should be overridden
+            String overriddenValue = HentaiConfigUtils.setOverriddenValue(context, namespace, name);
+            if (overriddenValue != null) {
+                String[] parts = overriddenValue.split("=");
+                if (parts.length == 2) {
+                    name = parts[0];
+                    value = parts[1];
+                }
+            }
             return sNameValueCache.putStringForUser(resolver, createCompositeName(namespace, name),
                     value, null, makeDefault, resolver.getUserId(),
                     DEFAULT_OVERRIDEABLE_BY_RESTORE);
