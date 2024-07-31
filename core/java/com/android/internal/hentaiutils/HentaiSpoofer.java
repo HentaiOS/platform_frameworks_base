8dbf5740ce4f8eaa2a51524a36f979eb26b8558f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-07-31 20:58:50 +0000

    hentaiSpoofer: Introduce native spoofer
    Nuke the keystore attestation blocker for now.
    
    Change-Id: I76e2f60944888921831abad2086ee21418f6387f
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>

--- core/java/com/android/internal/hentaiutils/HentaiSpoofer.java
+++ core/java/com/android/internal/hentaiutils/HentaiSpoofer.java
@@ -39,6 +39,10 @@ public final class HentaiSpoofer {
     private static final String PACKAGE_FINSKY = "com.android.vending";
     private static final String PROCESS_UNSTABLE = "com.google.android.gms.unstable";
 
+    private static final String FIRST_API_LEVEL = "persist.helluva.spoofer.first_api_level";
+    private static final String SECURITY_PATCH = "persist.helluva.spoofer.security_patch";
+    private static final String BUILD_ID = "persist.helluva.spoofer.build_id";
+
     private static volatile boolean sIsSvt = false;
     private static volatile boolean sIsGms = false;
     private static volatile boolean sIsFinsky = false;
@@ -81,8 +85,17 @@ public final class HentaiSpoofer {
         return false;
     }
 
+    private static void setSystemProperty(String name, String value) {
+        try {
+            SystemProperties.set(name, value);
+            Log.d(TAG, "Set system prop " + name + "=" + value);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to set system prop " + name + "=" + value, e);
+        }
+    }
+
     // Play Integrity
-    private static void spoofGmsAttest(Context context) {
+    private static void spoofBuildGmsAttest(Context context) {
         PackageManager pm = context.getPackageManager();
 
         try {
@@ -96,6 +109,7 @@ public final class HentaiSpoofer {
                 if (sCertifiedProps != null) {
                     sIsSvt = true;
 
+                    spoofSystemGmsAttest(context, resources);
                     for (String prop : buildProperties) {
                         int index = Arrays.asList(buildProperties).indexOf(prop);
                         if (index < sCertifiedProps.length && sCertifiedProps[index] != null && !sCertifiedProps[index].isEmpty()) {
@@ -103,7 +117,7 @@ public final class HentaiSpoofer {
                         }
                     }
                 } else {
-                    Log.d(TAG, "sCertifiedProps is null");
+                    Log.d(TAG, "certifiedBuildProperties is null");
                 }
             } else {
                 Log.d(TAG, "Resource ID is not found");
@@ -111,8 +125,28 @@ public final class HentaiSpoofer {
         } catch (PackageManager.NameNotFoundException e) {
             Log.i(TAG, "Error accessing resources for '" + PACKAGE_SVT + "': " + e.getMessage());
         }
+    }
 
-        return;
+    private static void spoofSystemGmsAttest(Context context, Resources resources) {
+        int resourceId = resources.getIdentifier("certifiedSystemProperties", "array", PACKAGE_SVT);
+
+        if (resourceId != 0) {
+            String[] sCertifiedProps = resources.getStringArray(resourceId);
+            String[] systemPropsKeys = {FIRST_API_LEVEL, SECURITY_PATCH, BUILD_ID};
+
+            if (sCertifiedProps != null) {
+                for (String prop : systemPropsKeys) {
+                    int index = Arrays.asList(systemPropsKeys).indexOf(prop);
+                    if (index < sCertifiedProps.length && sCertifiedProps[index] != null && !sCertifiedProps[index].isEmpty()) {
+                        setSystemProperty(prop, sCertifiedProps[index]);
+                    }
+                }
+            } else {
+                Log.d(TAG, "certifiedSystemProperties is null");
+            }
+        } else {
+            Log.d(TAG, "Resource ID is not found");
+        }
     }
 
     // Product Spoofing
@@ -143,8 +177,6 @@ public final class HentaiSpoofer {
         } catch (PackageManager.NameNotFoundException e) {
             Log.i(TAG, "Error accessing resources for '" + PACKAGE_SPT + "': " + e.getMessage());
         }
-
-        return;
     }
 
     public static void initApplicationBeforeOnCreate(Context context) {
@@ -158,8 +190,7 @@ public final class HentaiSpoofer {
         // Play Integrity
         if (packageName.equals(PACKAGE_GMS) &&
                 processName.equals(PROCESS_UNSTABLE)) {
-            sIsGms = true;
-            spoofGmsAttest(context);
+            spoofBuildGmsAttest(context);
         }
 
         if (packageName.equals(PACKAGE_FINSKY)) {
@@ -169,19 +200,6 @@ public final class HentaiSpoofer {
         // Product Spoofing
         if (spoofProcesses(processName, PROCESSES_SPT)) {
             spoofGms(context);
-        }
-    }
-
-    private static boolean isCallerSafetyNet() {
-        return sIsGms && Arrays.stream(Thread.currentThread().getStackTrace())
-                .anyMatch(elem -> elem.getClassName().contains("DroidGuard"));
-    }
-
-    public static void onEngineGetCertificateChain() {
-        // Check stack for SafetyNet or Play Integrity
-        if (sIsSvt && (isCallerSafetyNet() || sIsFinsky)) {
-            Log.i(TAG, "Blocked key attestation sIsGms=" + sIsGms + " sIsFinsky=" + sIsFinsky);
-            throw new UnsupportedOperationException();
         }
     }
 }
